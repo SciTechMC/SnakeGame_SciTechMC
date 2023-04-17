6de98cab5f93332aa2b3fd60b7ba3d330d896f98
SciTechMC <79587685+SciTechMC@users.noreply.github.com>
2023-04-17 10:47:53 +0200

    Code updated
    to finish:
    apple ==> planet
    background change
    easygui for name

--- snake.py
+++ snake.py
@@ -1,44 +1,51 @@
-import pgzrun
-import os
+#variables besoin pour score board:
+#playerName
+#playTimeStop
+#score
+
 import pygame
+import time
 
 TITLE = "Snake"
 
-WIDTH  = 802
-HEIGHT = 602
+WIDTH  = 800
+HEIGHT = 600
 
 
 import random
-import time
+#background
+background = pygame.image.load('.\images\space-bg.jpg')
+
 
 #player
-snake=Actor('snake.png')
-snake.width = 24
-snake.height = 24
-
-snake.x=400
-snake.y=300
+ship = Actor('rocket_64.png')
+ship.x=400
+ship.y=300
 
 #apple
 apple=Actor('apple.png')
-apple.width = 25
-apple.height = 25
+apple.width = 64
+apple.height = 64
 
 apple.x=random.randint(30,WIDTH-30)
 apple.y=random.randint(30,HEIGHT-30)
 
 #other
-direction = "R"
+playerName = str(input("Le nom du joueur: "))
+
+time.sleep(5)
+
+direction = "U"
 
 score = 0
 
-snakeSpeed = 5
+shipSpeed = 5
 
 pause = False
 
 appleTot = 0
 
-snakeSpeedP = 5
+shipSpeedP = 5
 
 minSpeedP = 0
 
@@ -54,215 +61,251 @@ restart = False
 
 scoreWin = False
 
-#Rest of code -----------------
+timer = 0
 
+playTimeStop = 0
+
+gameTime = 0
+
+tauxDeJeux = 3
+#Rest of code -----------------
+    
 def draw():
     #global variables ------------
     global gameOver
     global score
     global restart
-    global snakeSpeed
+    global shipSpeed
     global minSpeed
     global pause
-    global minSpeedP
-    global snakeSpeedP
     global direction
     global playTime
-    global scoreWin
+    global playTimeStop
+    global tauxDeJeux
     #global variables ------------
+    global pause
+    global gameOver
+    global gameTime
     
-    screen.clear()
+    if pause == False or gameOver == False:
     
-    # Draw horizontal lines
-    for y in range(1, HEIGHT, 25):
-        pygame.draw.line(screen.surface, (255, 255, 255), (0, y), (WIDTH, y))
-    
-    # Draw vertical lines
-    for x in range(1, WIDTH, 25):
-        pygame.draw.line(screen.surface, (255, 255, 255), (x, 0), (x, HEIGHT))
-        
-
-    
-    if (gameOver != True):
-        scorefunt()
-        snake.draw()
-        apple.draw()
-        speedDraw()
-        minSpeedDraw()
-        
-        if snake.x >= 800:
-            gameOver = True
-
-        elif snake.x <= 0:
-            snake.y = 800
-            gameOver = True
-                
-        elif snake.y >= 590:
-            gameOver = True
-                
-        elif snake.y <= 0:
-            gameOver = True
-            
-        if score >= 1000000000:
-            score = 999999999
-            
-        if scoreWin == True:
-            screen.clear()
-            screen.draw.text('You Won!')
-#        if score >= 10:
-#            minSpeed = 5
-#        elif score >= 20:
-#            minSpeed = 10
-#        elif score >= 60:
-#            minSpeed = 15
-#        elif score >= 200:
-#            scoreWinFunt()
-
-    else:
         screen.clear()
-        drawGame()
+        screen.blit(background, (0, 0))
+            
+        screen.draw.text('Time: ' + str(playTime),(360,30),width=180,lineheight=20, color="Green")
         
-        if restart == True:
+        minSpeedChange()
+        
+        if (gameOver != True):
+            scorefunt()
+            ship.draw()
+            apple.draw()
+            speedDraw()
+            tauxDeJeuxfont()
             
-            screen.clear()
-            createApple()
-            direction = "R"
-            pause = False
-            gameOver = False
-            restart = False
-            snake.x = 400
-            snake.y = 300
-            score = 0
-            snakeSpeed = 5
-            minSpeed = -1
-            playTime = 0
-            
-    if pause == True and gameOver == False:
-        screen.draw.text('Paused' ,(275,275),width=180,fontsize=100,lineheight=20, color="Red")
+            if ship.x >= 800:
+                gameOver = True
 
-def update():
+            elif ship.x <= 0:
+                ship.y = 800
+                gameOver = True
+                    
+            elif ship.y >= 590:
+                gameOver = True
+                    
+            elif ship.y <= 0:
+                gameOver = True
+                
+            if score >= 1000000000:
+                score = 999999999
+                
+            if scoreWin == True:
+                screen.clear()
+                screen.draw.text('You Won!')
+
+        else:
+            screen.clear()
+            drawGame()
+            playTimeStop = playTime
+            
+            if restart == True:
+                
+                screen.clear()
+                createApple()
+                ion = "R"
+                pause = False
+                gameOver = False
+                restart = False
+                ship.x = 400
+                ship.y = 300
+                score = 0
+                shipSpeed = 5
+                minSpeed = -1
+                playTime = 0
+                tauxDeJeux -= 1
+                if tauxDeJeux <= -1:
+                    pygame.quit()
+                if score >= 1000000000:
+                    score = 999999999
+                
+        if pause == True and gameOver == False:
+            screen.draw.text('Paused' ,(275,275),width=180,fontsize=100,lineheight=20, color="Red")
+
+def update(dt):
     global scores
     global circles
-    global snake
-    global snakeSpeed
+    global ship
+    global shipSpeed
     global score
     global minSpeed
-    
-    if direction == "U":
-        snake.y -= snakeSpeed
-    elif direction == "D":
-        snake.y += snakeSpeed
-    elif direction == "L":
-        snake.x -= snakeSpeed
-    elif direction == "R":
-        snake.x += snakeSpeed
+    global timer
+    global pause
+    global gameOver
+    global playTimeStop
+    global tauxDeJeux
         
-    if snake.colliderect(apple):
-        apple.x=random.randint(30,WIDTH-30)
-        apple.y=random.randint(30,HEIGHT-30)
+    if gameOver == False:
         
-        if snakeSpeed == -1:
-            score += 2
-        elif snakeSpeed <= 5:
-            score += 1
-        elif snakeSpeed <= 10:
-            score += 4
-        elif snakeSpeed <= 15:
-            score += 8
-        else:
-            score += 1
-    
-    if snakeSpeed <= minSpeed:
-        snakeSpeed = minSpeed
+        timer += dt
+        if timer > 1:
+            timer = 0
+            playTimeFunt()
+        
+        if direction == "U":
+            ship.y -= shipSpeed
+        elif direction == "D":
+            ship.y += shipSpeed
+        elif direction == "L":
+            ship.x -= shipSpeed
+        elif direction == "R":
+            ship.x += shipSpeed
             
-    if snakeSpeed <= -2:
-        print("reverting to previous value")
-        snakeSpeed = -1
-    elif snakeSpeed >= 16:
-        snakeSpeed = 15
+        if ship.colliderect(apple):
+            apple.x=random.randint(30,WIDTH-30)
+            apple.y=random.randint(30,HEIGHT-30)
+            
+            if shipSpeed == -1:
+                score += 2
+            elif shipSpeed <= 5:
+                score += 1
+            elif shipSpeed <= 10:
+                score += 2
+            elif shipSpeed <= 15:
+                score += 4
+            else:
+                score += 1
         
-#    if score >= 10:
-#        minSpeed = 5
-#    elif score >= 20:
-#        minSpeed = 10
-#    elif score >= 60:
-#        minSpeed = 15
-#    elif score >= 200:
-#        scoreWinFunt()
+        if shipSpeed < minSpeed:
+            shipSpeed = minSpeed
+                
+        if shipSpeed <= -2:
+            shipSpeed = -1
+        elif shipSpeed >= 16:
+            shipSpeed = 15
+    
+    if gameOver != True or pause != True:
+        gameTime = timer
 
 def on_key_down(key):
     global score
     global direction
     global gameOver
-    global snakeSpeed
+    global shipSpeed
     global restart
     global pause
     global minSpeedP
-    global snakeSpeedP
+    global shipSpeedP
     global minSpeed
+    global ship
+    global gameOver
+    global playTimeStop
+    global tauxDeJeux
     
-    
-    if (key == keys.UP) and direction != "D":
-        direction = "U"
-    elif (key == keys.DOWN) and direction != "U":
-        direction = "D"
-    elif (key == keys.LEFT) and direction != "R":
-        direction = "L"
-    elif (key == keys.RIGHT) and direction != "L":
-        direction = "R"
+    if pause == False or gameOver == False:
         
-    elif (key == keys.S):
-        gameOver = True
-        
-    elif (key == keys.KP0):
-        snakeSpeed = - 1
-        
-    elif (key == keys.KP1):
-        snakeSpeed = 0
-        
-    elif (key == keys.KP2):
-        snakeSpeed = 5
-        
-    elif (key == keys.KP3):
-        snakeSpeed = 10
+        if (key == keys.UP):
+            direction = "U"
+            ship.angle = 0
+        elif (key == keys.DOWN):
+            direction = "D"
+            ship.angle = 180
+        elif (key == keys.LEFT):
+            direction = "L"
+            ship.angle = 90
+        elif (key == keys.RIGHT):
+            direction = "R"
+            ship.angle = 270
             
-    elif (key == keys.KP4):
-        snakeSpeed = 15
+        elif (key == keys.S):
+            gameOver = True
+            
+        elif (key == keys.KP0):
+            shipSpeed = - 1
+            
+        elif (key == keys.KP1):
+            shipSpeed = 0
+            
+        elif (key == keys.KP2):
+            shipSpeed = 5
+            
+        elif (key == keys.KP3):
+            shipSpeed = 10
+                
+        elif (key == keys.KP4):
+            shipSpeed = 15
+            
+        elif (key == keys.KP5):
+            shipSpeed -= 1
         
-    elif (key == keys.KP5):
-        snakeSpeed -= 1
-    
-    elif (key == keys.KP8):
-        snakeSpeed += 1
+        elif (key == keys.KP8):
+            shipSpeed += 1
+            
+        elif (key == keys.R):
+            if gameOver == True:
+                restart = True
+            
+        elif (key == keys.F9):
+            score = 999999999
         
-    elif (key == keys.BACKSPACE):
-        if gameOver == True:
-            restart = True
-        
-    elif (key == keys.F9):
-        score += 999999999
-        
-    elif (key == keys.P):
-        pause = not pause
-        if pause == True and gameOver == False:
-            minSpeedP = minSpeed
-            snakeSpeedP = snakeSpeed
-            minSpeed = 0
-            snakeSpeed = 0
-        else:
-            minSpeed = minSpeedP
-            snakeSpeed = snakeSpeedP
+        elif (key == keys.F10):
+            tauxDeJeux = 3
+            
+        elif (key == keys.F11):
+            tauxDeJeux = 999999999
+            
+        elif (key == keys.P):
+            pause = not pause
+            if pause == True and gameOver == False:
+                minSpeedP = minSpeed
+                shipSpeedP = shipSpeed
+                minSpeed = 0
+                shipSpeed = 0
+            else:
+                minSpeed = minSpeedP
+                shipSpeed = shipSpeedP
 
-
+def tauxDeJeuxfont():
+    global tauxDeJeux
+    if tauxDeJeux == 3:
+        screen.draw.text('Jeux restants: ' + str(tauxDeJeux),(30, 580), color="green")
+    if tauxDeJeux == 2:
+        screen.draw.text('Jeux restants: ' + str(tauxDeJeux),(30, 580), color="orange")
+    if tauxDeJeux == 1:
+        screen.draw.text('Jeux restants: ' + str(tauxDeJeux),(30, 580), color="orange")
+    if tauxDeJeux == 0:
+        screen.draw.text('Jeux restants: ' + str(tauxDeJeux),(30, 580), color="red")
 def drawGame():
     global minSpeed
     global score
-    screen.draw.text('GAME OVER',(350,200),width=180,lineheight=5)
-    screen.draw.text('Your score: ' + str(score),(350,300),width=360,lineheight=5)
-    screen.draw.text('Your speed: ' + str(snakeSpeed),(350,400),width=180,lineheight=5)
+    global playTimeStop
+    global tauxDeJeux
+    screen.draw.text('GAME OVER',(350,100),width=180,lineheight=5, color="red")
+    screen.draw.text('Your score: ' + str(score),(350,200),width=360,lineheight=5)
+    screen.draw.text('Your speed: ' + str(shipSpeed),(350,300),width=180,lineheight=5)
+    screen.draw.text('Play Time: ' + str(playTime),(350,400),width=180,lineheight=5)
 def speedDraw():
-    global snakeSpeed
-    screen.draw.text(str(snakeSpeed),(760,30))
+    global shipSpeed
+    screen.draw.text('Speed: ' + str(shipSpeed),(700,30))
     
 def createApple():
     global apple
@@ -271,46 +314,44 @@ def createApple():
     
 def scorefunt():
     global score
-    screen.draw.text(str(score), (35,30))
-
-def scoreWinFunt():
-    scoreWin = True
+    screen.draw.text('Score: ' + str(score), (35,30))
 
 def minSpeedDraw():
     global minSpeed
-    screen.draw.text(str(minSpeed), (35, 580))
+    screen.draw.text('Min Speed: ' + str(minSpeed), (35, 570))
+      
+def playTimeFunt():
+    global playTime
+    
+    playTime += 1
+    
+def minSpeedChange():
+    global minSpeed
+    global playTime
+    
+    if playTime == 20:
+        minSpeed = 6
+    elif playTime == 30:
+        minSpeed = 7
+    elif playTime == 40:
+        minSpeed = 8
+    elif playTime == 50:
+        minSpeed = 9
+    elif playTime == 60:
+        minSpeed = 10
+    elif playTime == 70:
+        minSpeed = 11
+    elif playTime == 80:
+        minSpeed = 12
+    elif playTime == 90:
+        minSpeed = 13
+    elif playTime == 100:
+        minSpeed = 14
+    elif playTime == 110:
+        minSpeed = 15
         
-#    if pause == False:
-#        clock.schedule_interval(playTimefunt, 1.0)
-#    
-#        clock.schedule_interval(minSpeedfunt, 15.0)
-#        if minSpeed <= 5:
-#            screen.draw.text('Minimum Speed: ' + str(minSpeed),(35,570),width=180,lineheight=20, color="Green")
-#        elif minSpeed <= 10:
-#            screen.draw.text('Minimum Speed: ' + str(minSpeed),(35,570),width=180,lineheight=20, color="Orange")
-#        elif minSpeed <= 15:
-#            screen.draw.text('Minimum Speed: ' + str(minSpeed),(35,570),width=180,lineheight=20, color="Red")
+    elif playTime == 150:
+        minSpeed = 16
     
-
-#def playTimefunt():
-#    global winStatus
-#    global playTime
-#    
-#    clock.schedule_interval(playTimefunt, 1.0)
-#    screen.draw.text('Time: ' + str(playTime),(350,30),width=180,lineheight=20, color="Green")
-#    print(playTime)
-#    
-#    if playTime >= 3600:
-#        winStatus = True
-#    elif playTime < 3600:
-#        playTime += 1
     
-#    clock.unschedule(playTimefunt)
-#def minSpeedfunt():
-#    global minSpeed
-#    if minSpeed >= 15:
-#        minSpeed = 15
-#    else:
-#        minSpeed += 1
     
-#   clock.unschedule(minSpeedfunt)
